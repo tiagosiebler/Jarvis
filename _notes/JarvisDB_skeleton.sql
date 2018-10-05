7ff999e3733e53abae1342cd854295affcb7712a
TS <ts@github.com>
2018-10-05 12:36:46 +0100

    Update MariaDB JarvisDB skeleton.

--- _notes/JarvisDB_skeleton.sql
+++ _notes/JarvisDB_skeleton.sql
@@ -1,45 +1,41 @@
--- Host: localhost
--- Generation Time: Oct 09, 2017 at 04:05 PM
--- Server version: 5.7.17
--- PHP Version: 7.1.7
+/*
+ Navicat Premium Data Transfer
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+ Source Server         : JarvisDB-tsiebler
+ Source Server Type    : MariaDB
+ Source Server Version : 50556
+ Source Host           : slackbot:3306
+ Source Schema         : jarvisdb
 
+ Target Server Type    : MariaDB
+ Target Server Version : 50556
+ File Encoding         : 65001
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+ Date: 05/10/2018 12:33:04
+*/
 
---
--- Database: `jarvis`
---
-CREATE DATABASE IF NOT EXISTS `jarvisdb` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `jarvisdb`;
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `lu_slack_channels`
---
-
+-- ----------------------------
+-- Table structure for lu_slack_channels
+-- ----------------------------
+DROP TABLE IF EXISTS `lu_slack_channels`;
 CREATE TABLE `lu_slack_channels` (
-  `slack_channel_id_int` int(11) NOT NULL,
+  `slack_channel_id_int` int(11) NOT NULL AUTO_INCREMENT,
   `slack_channel_id` varchar(255) NOT NULL,
   `slack_channel_name` varchar(255) NOT NULL,
   `slack_channel_visibility` varchar(10) NOT NULL,
-  `dt_last_resolved` datetime NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `dt_last_resolved` datetime NOT NULL,
+  PRIMARY KEY (`slack_channel_id_int`)
+) ENGINE=InnoDB AUTO_INCREMENT=96 DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `lu_slack_users`
---
-
+-- ----------------------------
+-- Table structure for lu_slack_users
+-- ----------------------------
+DROP TABLE IF EXISTS `lu_slack_users`;
 CREATE TABLE `lu_slack_users` (
-  `slack_user_id_int` int(11) NOT NULL,
+  `slack_user_id_int` int(11) NOT NULL AUTO_INCREMENT,
   `slack_user_id` varchar(255) NOT NULL,
   `slack_username` varchar(255) NOT NULL,
   `slack_usertitle` varchar(255) DEFAULT NULL,
@@ -50,17 +46,29 @@ CREATE TABLE `lu_slack_users` (
   `real_name` varchar(255) DEFAULT NULL,
   `dt_last_resolved` datetime NOT NULL,
   `sf_username` varchar(255) DEFAULT NULL,
-  `sf_user_id` varchar(255) DEFAULT NULL
+  `sf_user_id` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`slack_user_id`),
+  UNIQUE KEY `slackUserIntID` (`slack_user_id_int`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=546 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for sf_auth
+-- ----------------------------
+DROP TABLE IF EXISTS `sf_auth`;
+CREATE TABLE `sf_auth` (
+  `sf_auth_id` int(11) NOT NULL AUTO_INCREMENT,
+  `sf_user_id` varchar(255) NOT NULL,
+  `dt_authed` datetime NOT NULL,
+  `sf_token` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`sf_auth_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `slack_threads`
---
-
+-- ----------------------------
+-- Table structure for slack_threads
+-- ----------------------------
+DROP TABLE IF EXISTS `slack_threads`;
 CREATE TABLE `slack_threads` (
-  `thread_id` int(11) NOT NULL,
+  `thread_id` int(11) NOT NULL AUTO_INCREMENT,
   `thread_ts` varchar(255) NOT NULL,
   `dt_added` datetime NOT NULL,
   `slack_user_id` varchar(255) NOT NULL,
@@ -70,17 +78,16 @@ CREATE TABLE `slack_threads` (
   `sf_post_id` varchar(255) DEFAULT NULL,
   `sf_post_created` tinyint(1) NOT NULL,
   `sf_post_url` varchar(255) DEFAULT NULL,
-  `sf_should_sync` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `sf_should_sync` tinyint(1) NOT NULL,
+  PRIMARY KEY (`thread_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3011 DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `stats_posts`
---
-
+-- ----------------------------
+-- Table structure for stats_posts
+-- ----------------------------
+DROP TABLE IF EXISTS `stats_posts`;
 CREATE TABLE `stats_posts` (
-  `message_id` int(11) NOT NULL,
+  `message_id` int(11) NOT NULL AUTO_INCREMENT,
   `message_ts` varchar(255) NOT NULL,
   `message_dt` datetime NOT NULL,
   `thread_ts` varchar(255) DEFAULT NULL,
@@ -89,62 +96,40 @@ CREATE TABLE `stats_posts` (
   `post_url` varchar(255) DEFAULT NULL,
   `slack_user_id` varchar(255) NOT NULL,
   `slack_channel_id` varchar(255) NOT NULL,
-  `sf_case` int(11) DEFAULT NULL
+  `sf_case` int(11) DEFAULT NULL,
+  PRIMARY KEY (`message_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=79490 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for test1
+-- ----------------------------
+DROP TABLE IF EXISTS `test1`;
+CREATE TABLE `test1` (
+  `t1` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Indexes for dumped tables
---
+-- ----------------------------
+-- View structure for rob
+-- ----------------------------
+DROP VIEW IF EXISTS `rob`;
+CREATE ALGORITHM=UNDEFINED DEFINER=`analytics`@`%` SQL SECURITY DEFINER VIEW `rob` AS select `stats_posts`.`message_id` AS `message_id`,`stats_posts`.`message_ts` AS `message_ts`,`stats_posts`.`message_dt` AS `message_dt`,`stats_posts`.`thread_ts` AS `thread_ts`,`stats_posts`.`post_type` AS `post_type`,`stats_posts`.`message_text` AS `message_text`,`stats_posts`.`post_url` AS `post_url`,`stats_posts`.`slack_user_id` AS `slack_user_id`,`stats_posts`.`slack_channel_id` AS `slack_channel_id`,`stats_posts`.`sf_case` AS `sf_case` from `stats_posts` limit 10;
 
---
--- Indexes for table `lu_slack_channels`
---
-ALTER TABLE `lu_slack_channels`
-  ADD PRIMARY KEY (`slack_channel_id_int`);
+-- ----------------------------
+-- View structure for slack_missing_answer
+-- ----------------------------
+DROP VIEW IF EXISTS `slack_missing_answer`;
+CREATE ALGORITHM=UNDEFINED DEFINER=`analytics`@`%` SQL SECURITY DEFINER VIEW `slack_missing_answer` AS select `slack_post_q`.`message_id` AS `message_id`,`slack_post_q`.`message_ts` AS `message_ts`,`slack_post_q`.`message_dt` AS `message_dt`,`slack_post_q`.`thread_ts` AS `thread_ts`,`slack_post_q`.`post_type` AS `post_type`,`slack_post_q`.`message_text` AS `message_text`,`slack_post_q`.`post_url` AS `post_url`,`slack_post_q`.`slack_user_id` AS `slack_user_id`,`slack_post_q`.`slack_channel_id` AS `slack_channel_id`,`slack_post_q`.`sf_case` AS `sf_case` from `slack_post_q` where (not(`slack_post_q`.`message_ts` in (select `slack_post_a`.`thread_ts` from `slack_post_a`)));
 
---
--- Indexes for table `lu_slack_users`
---
-ALTER TABLE `lu_slack_users`
-  ADD PRIMARY KEY (`slack_user_id`),
-  ADD UNIQUE KEY `slackUserIntID` (`slack_user_id_int`) USING BTREE;
+-- ----------------------------
+-- View structure for slack_post_a
+-- ----------------------------
+DROP VIEW IF EXISTS `slack_post_a`;
+CREATE ALGORITHM=UNDEFINED DEFINER=`analytics`@`%` SQL SECURITY DEFINER VIEW `slack_post_a` AS select `stats_posts`.`message_id` AS `message_id`,`stats_posts`.`message_ts` AS `message_ts`,`stats_posts`.`message_dt` AS `message_dt`,`stats_posts`.`thread_ts` AS `thread_ts`,`stats_posts`.`post_type` AS `post_type`,`stats_posts`.`message_text` AS `message_text`,`stats_posts`.`post_url` AS `post_url`,`stats_posts`.`slack_user_id` AS `slack_user_id`,`stats_posts`.`slack_channel_id` AS `slack_channel_id`,`stats_posts`.`sf_case` AS `sf_case` from `stats_posts` where (`stats_posts`.`post_type` = 'reply');
 
---
--- Indexes for table `slack_threads`
---
-ALTER TABLE `slack_threads`
-  ADD PRIMARY KEY (`thread_id`);
+-- ----------------------------
+-- View structure for slack_post_q
+-- ----------------------------
+DROP VIEW IF EXISTS `slack_post_q`;
+CREATE ALGORITHM=UNDEFINED DEFINER=`analytics`@`%` SQL SECURITY DEFINER VIEW `slack_post_q` AS select `stats_posts`.`message_id` AS `message_id`,`stats_posts`.`message_ts` AS `message_ts`,`stats_posts`.`message_dt` AS `message_dt`,`stats_posts`.`thread_ts` AS `thread_ts`,`stats_posts`.`post_type` AS `post_type`,`stats_posts`.`message_text` AS `message_text`,`stats_posts`.`post_url` AS `post_url`,`stats_posts`.`slack_user_id` AS `slack_user_id`,`stats_posts`.`slack_channel_id` AS `slack_channel_id`,`stats_posts`.`sf_case` AS `sf_case` from `stats_posts` where (`stats_posts`.`post_type` = 'post');
 
---
--- Indexes for table `stats_posts`
---
-ALTER TABLE `stats_posts`
-  ADD PRIMARY KEY (`message_id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `lu_slack_channels`
---
-ALTER TABLE `lu_slack_channels`
-  MODIFY `slack_channel_id_int` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `lu_slack_users`
---
-ALTER TABLE `lu_slack_users`
-  MODIFY `slack_user_id_int` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `slack_threads`
---
-ALTER TABLE `slack_threads`
-  MODIFY `thread_id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `stats_posts`
---
-ALTER TABLE `stats_posts`
-  MODIFY `message_id` int(11) NOT NULL AUTO_INCREMENT;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+SET FOREIGN_KEY_CHECKS = 1;
