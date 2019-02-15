6afee788fdb88a15c4db02209ab7e1e4c7d60ded
TS <ts@github.com>
2019-02-15 11:58:38 +0000

    some cleaning, linting & ES6 use

--- skills/expirySchedule.js
+++ skills/expirySchedule.js
@@ -1,100 +1,125 @@
 // custom modules with some hardcoded values/references
-var scraper = require('../submodules/scraper.js');
+const scraper = require('../submodules/scraper.js');
 
-// scope of where these commands will trigger (anywhere the bot is, right now)
-var listenScope = {
-	"everywhere": 'ambient,direct_message,direct_mention,mention',
-}
+// TODO: move to more general attachment generator
+const generatePlainAttachmentStr = (title, str) => {
+  return {
+    attachments: [
+      {
+        fallback: title,
+        title: title,
+        text: str
+      }
+    ]
+  };
+};
 
-var generatePlainAttachmentStr = function(title, str){
-	var retAttachment = {
-		"attachments": [{
-			"fallback": title,
-			"title": title,
-			"text": str,
-		}]
-	};
-	return retAttachment;
-}
+// TODO: move to a more general attachment generator
+const generateExpirationAttachment = (
+  version,
+  statusStr,
+  releaseDate,
+  expirationDate,
+  url
+) => {
+  return {
+    attachments: [
+      {
+        fallback: `${version} : ${statusStr}`,
+        title: `<${url}|Expiration schedule for MicroStrategy ${version}>`,
+        fields: [
+          {
+            title: 'Version',
+            value: 'MicroStrategy ' + version,
+            short: true
+          },
+          {
+            title: 'Status',
+            value: statusStr,
+            short: true
+          },
+          {
+            title: 'Original Release',
+            value: releaseDate,
+            short: true
+          },
+          {
+            title: 'Expected Expiration*',
+            value: expirationDate,
+            short: true
+          }
+        ],
+        color: statusStr == 'Expired' ? '#D9232E' : '#36a64f'
+      },
+      {
+        text:
+          '* _Note: Expiration may occur later than the dates published above, based on product release schedules. However, dates will not be moved earlier than those posted above._',
+        mrkdwn_in: ['text']
+      }
+    ]
+  };
+};
 
-var generateExpirationAttachment = function(version, statusStr, releaseDate, expirationDate, url){
-	var retAttachment = {
-		"attachments": [
-				{
-				"fallback": version + " : " + statusStr,
-				"title": "<"+url+"|Expiration schedule for MicroStrategy "+version+">",
-	            "fields": [
-					{
-						"title": "Version",
-						"value": "MicroStrategy "+version,
-						"short": true
-					},
-					{
-						"title": "Status",
-						"value": statusStr,
-						"short": true
-					},
-					{
-						"title": "Original Release",
-						"value": releaseDate,
-						"short": true
-					},
-					{
-						"title": "Expected Expiration*",
-						"value": expirationDate,
-						"short": true
-					},
-				],
-				"color": "#36a64f",
-			},{
-            	"text": "* _Note: Expiration may occur later than the dates published above, based on product release schedules. However, dates will not be moved earlier than those posted above._",
-			    "mrkdwn_in": ["text"]
-			}
-		]
-	};
+const queryRegexArray = [
+  '(.*)support(.*)',
+  '(.*)expir(.*)',
+  '(.*)expiration(.*)'
+];
+const queryListenScope = 'direct_message,direct_mention,mention';
 
-	if(statusStr == 'Expired') retAttachment.attachments[0].color = "#D9232E";
-	return retAttachment;
-}
+const handleExpirationQueryMessage = (bot, message) => {
+  bot.startConversationInThread(message, (err, convo) => {
+    if (err) return false;
 
-module.exports = function(controller) {
-	controller.hears(['(.*)support(.*)','(.*)expir(.*)','(.*)expiration(.*)'], 'direct_message,direct_mention,mention', function(bot, message) {
+    // strip slack mentions, as they're misread as numbers /(\<@.*\>)/g
+    message.text = message.text.replace(/(<@.*>)/g, '');
+    //console.log("stripped mention: ", message.text);
 
-		console.log("caught possible question about whether a version's still supported:", message);
+    scraper.isQuestionOnVersionSupport(message.text, question => {
+      if (!question.isValid) {
+        //console.log("not a valid version support question: ");
+        return convo.stop();
+      }
 
-		bot.startConversationInThread(message, function(err, convo) {
-			if (!err) {
-				// strip slack mentions, as they're misread as numbers /(\<@.*\>)/g
-				message.text = message.text.replace(/(\<@.*\>)/g, "");
-				//console.log("stripped mention: ", message.text);
-				
-				scraper.isQuestionOnVersionSupport(message.text, function(question){
-					if(!question.isValid){
-						//console.log("not a valid version support question: ");
-						convo.stop();
-					
-						return;
-					}
-	
-					scraper.getExpirationForVersion(question.version, function(result){
-						if(result.error){
-							convo.say(generatePlainAttachmentStr("Error fetching expiration schedule for version "+question.version,result.errorMSG));
-							convo.activate();
+      scraper.getExpirationForVersion(question.version, result => {
+        if (result.error) {
+          convo.say(
+            generatePlainAttachmentStr(
+              'Error fetching expiration schedule for version ' +
+                question.version,
+              result.errorMSG
+            )
+          );
+          return convo.activate();
+          //console.log("error occurred in fetching expiration schedule for version ", question.version, result);
+        }
 
-							//console.log("error occurred in fetching expiration schedule for version ", question.version, result);
-						}else{
-							convo.say(generateExpirationAttachment(question.version, result.supportStatus, result.releaseDate, result.expirationDate, result.url));
-							convo.activate();
-							
-							//console.log("Expiration schedule for ", question.version, result);
-						}
-					});
-				});
-			}
-		});
-	});
+        convo.say(
+          generateExpirationAttachment(
+            question.version,
+            result.supportStatus,
+            result.releaseDate,
+            result.expirationDate,
+            result.url
+          )
+        );
+        convo.activate();
+        //console.log("Expiration schedule for ", question.version, result);
+      });
+    });
+  });
+};
 
+// listener for when Jarvis is asked: "@jarvis is 10.11 expired?"
+module.exports = controller => {
+  controller.hears(queryRegexArray, queryListenScope, (bot, message) => {
+    console.log(
+      `Caught possible question on whether a version's still supported: "${
+        message.text
+      }"`
+    );
 
-
-
+    handleExpirationQueryMessage(bot, message);
+    return true;
+  });
 };
