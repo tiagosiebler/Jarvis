dd61f0dc6283ffd496e870b2dc81f187f4570539
Daniel Zumbaugh <dzumbaugh@microstrategy.com>
2020-01-14 18:45:00 -0500

    eslint to format

--- skills/lookup.js
+++ skills/lookup.js
@@ -1,55 +1,53 @@
 const ExpressionList = require('../submodules/Regex/ExpressionList');
 const ping = require('ping');
 
-
-module.exports = controller => {
-    controller.hears(
-        [ExpressionList.lookup], 
-        'direct_message,direct_mention', 
-        async (bot, message) => {
-            bot.createConversation(message, async(err, convo) => {
-                if (err) return false;
-                let responseMessage;
-                let domain = message.match[1];
-
-                if(!domain){
-                    convo.say("Missing parameter. Please provide the domain name to lookup");
-                    convo.activate();
-                    return;
-                }
-                //Slack will automatically try to make anything ending in '.com' be a link
-                //This causes issue with message response formatting which needs to be corrected
-                if(domain[0] === '<'){ 
-                    // RegEx matches: <http:// and >
-                    var regex = /(<http:\/\/|>)/gi;
-                    domain = domain.replace(regex, '');
-                    domain = domain.split('|')[0];
-                }
-                let ip = await pingResult(domain);
-                console.log("pingResult: " + ip);
-                if(!ip){
-                    responseMessage = "Could not retreive IP of domain: " + 
-                        domain + 
-                        ". Try using the fully qualified domain name";
-                }else{
-                    responseMessage = `Resolved to IP: \`${ip}\``;
-                }
-
-                convo.say(responseMessage);
-                convo.activate();
-            });
-        }
-    );
+const pingResult = async host => {
+  let result = null;
+  try {
+    result = await ping.promise.probe(host);
+    result = result.numeric_host;
+  } catch (e) {
+    console.log(e);
+    return e;
+  }
+  return result;
 };
 
-const pingResult = async (host) => {
-    let result = null;
-    try{
-        result = await ping.promise.probe(host);
-        result = result.numeric_host;
-    }catch(e){
-        console.log(e);
-        return e;
+module.exports = controller => {
+  controller.hears(
+    [ExpressionList.lookup],
+    'direct_message,direct_mention',
+    async (bot, message) => {
+      bot.createConversation(message, async (err, convo) => {
+        if (err) return false;
+        let responseMessage;
+        let domain = message.match[1];
+
+        if (!domain) {
+          convo.say(
+            'Missing parameter. Please provide the domain name to lookup'
+          );
+          convo.activate();
+          return;
+        }
+        //Slack will automatically try to make anything ending in '.com' be a link
+        //This causes issue with message response formatting which needs to be corrected
+        if (domain[0] === '<') {
+          // RegEx matches: <http:// and >
+          domain = domain.replace(/(<http:\/\/|>)/gi, '');
+          domain = domain.split('|')[0];
+        }
+        let ip = await pingResult(domain);
+        console.log('pingResult: ' + ip);
+        if (!ip) {
+          responseMessage = `Could not retreive IP of domain: ${domain}. Try using the fully qualified domain name`;
+        } else {
+          responseMessage = `Resolved to IP: \`${ip}\``;
+        }
+
+        convo.say(responseMessage);
+        convo.activate();
+      });
     }
-    return result
-}
+  );
+};
