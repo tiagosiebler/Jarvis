43b90d805c219a73ee9bcaee28178e9546c2f29f
tiagosiebler <ts@github.com>
2020-05-07 12:07:18 +0100

    trace

--- submodules/extDB/index.js
+++ submodules/extDB/index.js
@@ -587,7 +587,7 @@ module.exports = class ExtDB extends MySQLPool {
       return bot.api.conversations.info(infoQuery, (err, response) => {
         if (!response || !response.ok) return reject(response);
         if (!response.channel) {
-          console.error(`getChannelInfoFromAPI() has an unhandled response type: ${JSON.stringify(response)}`);
+          console.error(`WARNING: getChannelInfoFromAPI() has an unhandled response type: ${JSON.stringify(response)}`);
         }
         return resolve({
           slack_channel_id: response.channel.id,
